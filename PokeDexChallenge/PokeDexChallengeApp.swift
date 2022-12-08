1e3fdf926f62f2659ba7b3e8f1445e30322e5aec
Rodrigo Sanseverino de Anhaia <52359836+RodrigoAnhaia@users.noreply.github.com>
2022-12-08 13:06:57 -0300

    Initial Commit

---
+++ PokeDexChallenge/PokeDexChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDexChallengeApp.swift
+//  PokeDexChallenge
+//
+//  Created by Rodrigo de Anhaia on 08/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PokeDexChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
